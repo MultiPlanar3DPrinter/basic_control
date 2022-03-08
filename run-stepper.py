62439d1b2bc85ee0f84f6924dbaa7edb80a878d3
Brendan Arciszewski <git@brendanarciszewski.ca>
2022-03-08 14:28:07 -0500

    improve objectification

--- run-stepper.py
+++ run-stepper.py
@@ -1,38 +1,28 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
-
-out1 = 22
-out2 = 17
-out3 = 27
-out4 = 23
-STEP_PINS = [out1, out4, out2, out3] # (black red green blue) or (1 4 2 3)
-# STEP_PINS = [27, 22, 23, 17]
-# STEP_PINS = [23, 17, 27, 22]
-# STEP_PINS = [17, 27, 22, 23]
-
-def setup():
-    GPIO.setmode( GPIO.BCM )
-    GPIO.setup( out1, GPIO.OUT )
-    GPIO.setup( out2, GPIO.OUT )
-    GPIO.setup( out3, GPIO.OUT )
-    GPIO.setup( out4, GPIO.OUT )
+import math
 
 
-def initialize():
-    GPIO.output( out1, GPIO.LOW )
-    GPIO.output( out2, GPIO.LOW )
-    GPIO.output( out3, GPIO.LOW )
-    GPIO.output( out4, GPIO.LOW )
+ROTATER_LENGTH = 10
+DEG_PER_STEP = 1.8
+TURN_GEAR_RATIO = 2
+TILT_GEAR_RATIO = 2*3
 
 
-def cleanup():
-    initialize()
+
+
+def cleanup(steppers):
+    for s in steppers:
+        s.disable()
     GPIO.cleanup()
 
 
 class Stepper:
-    def __init__(self, half_steps=False):
+    def __init__(self, pins, half_steps=False):
+        self.pins = pins
+        self._setup()
+        self.disable()
         if half_steps:
             sequence = [[] for _ in range(8)]
             sequence[0] = [1,0,0,0]
@@ -54,13 +44,35 @@ class Stepper:
             self.sequence = sequence
             self.scale = 1
 
+    def _setup(self):
+        for pin in self.pins:
+            GPIO.setup(pin, GPIO.OUT)
+
+    def disable(self):
+        self.cur_idx = None
+        for pin in self.pins:
+            GPIO.output(pin, GPIO.LOW)
+
+    def hold(self):
+        self.cur_idx = None
+        for pin in self.pins:
+            GPIO.output(pin, GPIO.HIGH)
+
     def _output_at_seq_idx(self, seq_idx):
         for pin_idx in range(4):
-            pin = STEP_PINS[pin_idx]
+            pin = self.pins[pin_idx]
             if self.sequence[seq_idx][pin_idx] != 0:
                 GPIO.output(pin, GPIO.HIGH)
             else:
                 GPIO.output(pin, GPIO.LOW)
+    
+    def _update_seq_idx(self, sign):
+        seq_len = len(self.sequence)
+        self.cur_idx += sign
+        if (self.cur_idx == seq_len):
+            self.cur_idx = 0
+        elif (self.cur_idx < 0):
+            self.cur_idx = seq_len - 1
 
     def rotate(self, steps):
         if steps < 0:
@@ -69,37 +81,47 @@ class Stepper:
             sign = 1
         steps = sign * steps * self.scale
 
-        seq_len = len(self.sequence)
-        seq_idx = 0
-        for _ in range(steps):
-            self._output_at_seq_idx(seq_idx)
+        if self.cur_idx is None:
+            self.cur_idx = 0
 
-            seq_idx += sign
-            if (seq_idx == seq_len):
-                seq_idx = 0
-            elif (seq_idx < 0):
-                seq_idx = seq_len - 1
+        for _ in range(steps):
+            self._update_seq_idx(sign)
+            self._output_at_seq_idx(self.cur_idx)
 
             # change this carefully, might encounter mechanical (motor) limit
             time.sleep(0.008)
 
+
+def angle(turn_stepper, tilt_stepper, i, j, k):
+    alpha = math.atan2(i, j)
+    beta = math.asin(k/ROTATER_LENGTH)
+    turn_stepper.rotate(alpha*TURN_GEAR_RATIO/DEG_PER_STEP)
+    tilt_stepper.rotate(beta*TILT_GEAR_RATIO/DEG_PER_STEP)
+
+
 def main():
-    setup()
-    initialize()
+    out1 = 22
+    out2 = 17
+    out3 = 27
+    out4 = 23
+    STEP_PINS = [out1, out4, out2, out3] # (black red green blue) or (1 4 2 3)
+    # STEP_PINS = [27, 22, 23, 17]
+    # STEP_PINS = [23, 17, 27, 22]
+    # STEP_PINS = [17, 27, 22, 23]
+
+    GPIO.setmode(GPIO.BCM)
+    turn = Stepper(STEP_PINS, half_steps=True)
+    tilt = Stepper(None, half_steps=True)
     print("will run in 0.5s")
     time.sleep(0.5)
     try:
-        Stepper(half_steps=True).rotate(500)
+        angle(turn, tilt, 0, 0, 0)
     except KeyboardInterrupt:
-        cleanup()
+        cleanup([turn, tilt])
         exit( 1 )
- 
-    cleanup()
+
+    cleanup([turn, tilt])
     exit( 0 )
-
-
-def angle():
-    pass
 
 
 if __name__ == "__main__":
